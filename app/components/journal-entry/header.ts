8af3fafc844dc65ae1ff9eb8e612afbb37b2fe74
Jonny Heemstra <jonathan.heemstra@gmail.com>
2020-10-18 14:33:43 -0700

    General header layout

--- app/components/journal-entry/header.ts
+++ app/components/journal-entry/header.ts
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 
 interface JournalEntryHeaderArgs {
   title: string;
+  subtitle: string;
 }
 
 export default class JournalEntryHeader extends Component<JournalEntryHeaderArgs> {}
