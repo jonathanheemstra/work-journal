f705f99662417a24ee80d79b943a2b5e30c8f256
Jonny Heemstra <jonathan.heemstra@gmail.com>
2020-10-18 18:32:06 -0700

    Remove post test and rename footer test to foot

--- tests/integration/components/foot-test.ts
+++ tests/integration/components/foot-test.ts
@@ -1,24 +1,25 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | post', function(hooks) {
+import hbs from 'htmlbars-inline-precompile';
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | foot', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{post}}`);
+    await render(hbs`{{foot}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#post}}
+      {{#foot}}
         template block text
-      {{/post}}
+      {{/foot}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
